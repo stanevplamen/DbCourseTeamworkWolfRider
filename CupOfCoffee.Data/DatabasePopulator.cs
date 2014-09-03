bb4f92006c3760edc1e376c6c576bb43d221ab2a
Plamen <stanev.plamen@gmail.com>
2014-09-03 21:25:39 +0300

    Just adding starting data in DatabasePopulator
    CupOfCoffee.Data - > DatabasePopulator

--- CupOfCoffee.Data/DatabasePopulator.cs
+++ CupOfCoffee.Data/DatabasePopulator.cs
@@ -61,6 +61,11 @@ namespace CupOfCoffee.Data
                 new Category { Id = 3, Name = "Alchohol"},
                 new Category { Id = 4, Name = "Cigarettes"},
                 new Category { Id = 5, Name = "Food"},
+                new Category { Id = 6, Name = "Snack"},
+                new Category { Id = 7, Name = "Vegerables"},
+                new Category { Id = 8, Name = "Fruits"},
+                new Category { Id = 9, Name = "Nuts"},
+                new Category { Id = 9, Name = "Nuts"},
             };
 
             return categories;
@@ -98,6 +103,17 @@ namespace CupOfCoffee.Data
                 new Product { Id = productId++, CategoryId = 5, Name = "7 Days Double", SellPrice = 0.7m},
                 new Product { Id = productId++, CategoryId = 5, Name = "Gorna Bania 0.5l", SellPrice = 1m},
                 new Product { Id = productId++, CategoryId = 5, Name = "Prestige Qdesh Sega ili Gorish", SellPrice = 3.2m},
+                new Product { Id = productId++, CategoryId = 6, Name = "Croissant 7Days", SellPrice = 1.1m},
+                new Product { Id = productId++, CategoryId = 6, Name = "Croissant Fresh Baked", SellPrice = 1.2m},
+                new Product { Id = productId++, CategoryId = 6, Name = "Toast sandwitch", SellPrice = 1.2m},
+                new Product { Id = productId++, CategoryId = 7, Name = "Аvocado", SellPrice = 3.5m},
+                new Product { Id = productId++, CategoryId = 7, Name = "Olives", SellPrice = 2.4m},
+                new Product { Id = productId++, CategoryId = 8, Name = "Lemon", SellPrice = 0.5m},
+                new Product { Id = productId++, CategoryId = 8, Name = "Banana", SellPrice = 0.5m},
+                new Product { Id = productId++, CategoryId = 8, Name = "Orange", SellPrice = 0.4m},
+                new Product { Id = productId++, CategoryId = 9, Name = "Almonds", SellPrice = 3.1m},
+                new Product { Id = productId++, CategoryId = 9, Name = "Peanuts", SellPrice = 2.1m},
+                new Product { Id = productId++, CategoryId = 9, Name = "Hazelnuts", SellPrice = 2.1m},
             };
 
             return products;
@@ -135,9 +151,15 @@ namespace CupOfCoffee.Data
         {
             List<Position> positions = new List<Position>() 
             {
-                new Position { Id = 1, Name = "Senior Waitress", BaseSalary = 600m},
-                new Position { Id = 2, Name = "Junior Waitress", BaseSalary = 400m},
-                new Position { Id = 3, Name = "Boss", BaseSalary = 500m},
+                new Position { Id = 1, Name = "Senior Waitress", BaseSalary = 1400m},
+                new Position { Id = 2, Name = "Junior Waitress", BaseSalary = 1100m},
+                new Position { Id = 3, Name = "General Director", BaseSalary = 2500m},
+                new Position { Id = 4, Name = "Manager", BaseSalary = 1500m},
+                new Position { Id = 5, Name = "Marketing", BaseSalary = 1500m},
+                new Position { Id = 6, Name = "Traider", BaseSalary = 1500m},
+                new Position { Id = 7, Name = "Buyer", BaseSalary = 1500m},
+                new Position { Id = 8, Name = "Hostess", BaseSalary = 1100m},
+                new Position { Id = 8, Name = "Barman", BaseSalary = 1100m},
             };
 
             return positions;
@@ -147,8 +169,21 @@ namespace CupOfCoffee.Data
             int employeeId = 1;
             List<Employee> employees = new List<Employee>() 
             {
-                new Employee { Id = employeeId++, Name = "Viki", PositionId = 1, HireDate = new DateTime(2013, 9, 12) },
-                new Employee { Id = employeeId++, Name = "Didi", PositionId = 2, HireDate = new DateTime(2014, 5, 20) },
+                new Employee { Id = employeeId++, Name = "Viki Hristova", PositionId = 1, HireDate = new DateTime(2012, 9, 12) },
+                new Employee { Id = employeeId++, Name = "Didi Jeliazkova", PositionId = 2, HireDate = new DateTime(2014, 5, 20) },
+                new Employee { Id = employeeId++, Name = "Iva Mihailova", PositionId = 2, HireDate = new DateTime(2013, 5, 20) },
+                new Employee { Id = employeeId++, Name = "Ceca Cvetanova", PositionId = 2, HireDate = new DateTime(2013, 5, 21) },
+                new Employee { Id = employeeId++, Name = "Pepa Pepinova", PositionId = 2, HireDate = new DateTime(2013, 7, 22) },
+                new Employee { Id = employeeId++, Name = "Gina Stoichkova", PositionId = 2, HireDate = new DateTime(2013, 3, 23) },
+                new Employee { Id = employeeId++, Name = "Didi Ivanova", PositionId = 2, HireDate = new DateTime(2014, 1, 24) },
+                new Employee { Id = employeeId++, Name = "Pano Hristov", PositionId = 3, HireDate = new DateTime(2011, 1, 24) },
+                new Employee { Id = employeeId++, Name = "Momchil Cvetanov", PositionId = 4, HireDate = new DateTime(2012, 2, 18) },
+                new Employee { Id = employeeId++, Name = "Milena Rangelova", PositionId = 5, HireDate = new DateTime(2013, 4, 24) },
+                new Employee { Id = employeeId++, Name = "Evgeni Stoinev", PositionId = 6, HireDate = new DateTime(2013, 5, 13) },
+                new Employee { Id = employeeId++, Name = "Hristo Pachev", PositionId = 7, HireDate = new DateTime(2013, 5, 14) },
+                new Employee { Id = employeeId++, Name = "Maria Mihova", PositionId = 8, HireDate = new DateTime(2013, 2, 14) },
+                new Employee { Id = employeeId++, Name = "Ivan Mihov", PositionId = 9, HireDate = new DateTime(2014, 3, 10) },
+                new Employee { Id = employeeId++, Name = "Mitko Ivanov", PositionId = 9, HireDate = new DateTime(2013, 4, 16) },
             };
 
             return employees;
